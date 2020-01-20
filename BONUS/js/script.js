b200f2aea9467f1f72e0f593bf5863fa9ff3c419
Marco <marco.carone@libero.it>
2020-01-20 21:01:07 +0100

    fix css

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -6,11 +6,13 @@ $(document).ready(
       if (visible == false) {
         $(".dropdown").fadeOut();
         $(this).find('.dropdown').fadeIn();
+        $(this).children('a').addClass('link-active');
         $(this).find("i").removeClass("fa-chevron-down");
         $(this).find("i").addClass("fa-chevron-up");
         visible = true;
       } else {
         $(this).find('.dropdown').fadeOut();
+        $('.parent-dropdown > a').removeClass('link-active');
         $(this).find("i").removeClass("fa-chevron-up");
         $(this).find("i").addClass("fa-chevron-down");
         visible = false;
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(
   function() {
     $(".parent-dropdown").hover(function() {
       $(this).find('ul').slideToggle();
+      
     });
   }
 );
