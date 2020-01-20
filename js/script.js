409593d51403a6a763ffdd3540a55f7f42de1213
Marco <marco.carone@libero.it>
2020-01-20 21:14:33 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ $(document).ready(
   function() {
     $(".parent-dropdown").hover(function() {
       $(this).find('ul').slideToggle();
-      
     });
+    $(".parent-dropdown").hover(
+      function() {
+        $(this).find("i").removeClass("fa-chevron-down");
+        $(this).find("i").addClass("fa-chevron-up");
+
+      },
+      function() {
+        $(this).find("i").removeClass("fa-chevron-down");
+        $(this).find("i").addClass("fa-chevron-up");
+        $(this).find("i").removeClass("fa-chevron-up");
+        $(this).find("i").addClass("fa-chevron-down");
+      },
+    );
   }
 );
