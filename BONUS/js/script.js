4dec28c111bb3ddce1420ca65b4a4bbc4656618e
Marco <marco.carone@libero.it>
2020-01-20 18:14:59 +0100

    bonus finito

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(
   function() {
+    var visible = false;
     $(".parent-dropdown").click(function() {
-      $(this).find('ul').slideToggle();
+
+      if (visible == false) {
+        $(".dropdown").fadeOut();
+        $(this).find('.dropdown').fadeIn();
+        $(this).find("i").removeClass("fa-chevron-down");
+        $(this).find("i").addClass("fa-chevron-up");
+        visible = true;
+      } else {
+        $(this).find('.dropdown').fadeOut();
+        $(this).find("i").removeClass("fa-chevron-up");
+        $(this).find("i").addClass("fa-chevron-down");
+        visible = false;
+      }
     });
   }
 );
